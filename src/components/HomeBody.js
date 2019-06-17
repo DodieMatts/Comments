518083f552c2305b00a65dd0ce558b7f6fd20754
Dodie <dodiematts@gmail.com>
2019-06-17 11:27:38 -0400

    Embedded video is formatted correctly and centered

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -4,13 +4,10 @@ class HomeBody extends Component {
   render () {
     return(
         <div className="middle-section">
-          <h3> Check out my Clips!</h3>
-            <div className="embed-responsive embed-responsive-16by9">
               <iframe width="600" height="600" className="embed-responsive-item" title="Slaying with Galaxy Skin"
                 src="https://www.youtube.com/embed/1VfduBy9NRY"
                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
               </iframe>
-              </div>
         </div>
     );
   }
