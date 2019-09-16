8f682cdab10ce5525585a7a087a021705c8ac76a
Dodie Matthew <dodiematts@gmail.com>
2019-09-16 18:29:18 -0400

    update

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -4,7 +4,6 @@ class HomeBody extends Component {
   render () {
     return(
         <div className="middle-section">
-          <h3> Check out my Clips!</h3>
             <div className="embed-responsive embed-responsive-16by9">
               <iframe width="600" height="600" className="embed-responsive-item" title="Slaying with Galaxy Skin"
                 src="https://www.youtube.com/embed/1VfduBy9NRY"
